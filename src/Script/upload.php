a0aa39f8fe094247b900c579bd253a4123f6f198
Kaputchino <andreasmulard@gmail.com>
2023-11-28 15:09:11 +0100

    POO

--- src/Script/upload.php
+++ src/Script/upload.php
@@ -1,25 +1,34 @@
 <?php
 namespace  App\Script;
+Class fileUploader {
+    public $directory;
+    
+    function __construct(?string $name) {
+        $this->directory = $name;
+    }
+    function upload(){
+        $target_file =basename($_FILES["fileToUpload"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-$target_file =basename($_FILES["fileToUpload"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-//Choisi le nouveau nom, prend le temps avtuel en miliseconde et ajoute un nb aléoa
-$newname = "img".floor(microtime(true) * 1000).rand(0,10000).'.'.$imageFileType;
-
-// verifie le bon format
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {      
-    $filename = $_FILES["fileToUpload"]["tmp_name"];
-      if (move_uploaded_file($filename,'uploads/'.$newname)) {
-        echo "L'image ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a été mise en ligne";
-      }else {
-        echo "Problème durant l'upload";
-      }
-  } else {
-      //si il y a un probleme
-    echo "Pas une image";
-  }
+        //Choisi le nouveau nom, prend le temps avtuel en miliseconde et ajoute un nb aléoa
+        //$newname = "img".floor(microtime(true) * 1000).rand(0,10000).'.'.$imageFileType;
+        $newname = "img".uniqid().$imageFileType;
+        // verifie le bon format
+        if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+          if($check !== false) {      
+            $filename = $_FILES["fileToUpload"]["tmp_name"];
+              if (move_uploaded_file($filename,$directory.$newname)) {
+                echo "L'image ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a été mise en ligne";
+                  return $filename.$directory.$newname;
+              }else {
+                echo "Problème durant l'upload";
+              }
+          } else {
+              //si il y a un probleme
+            echo "Pas une image";
+          }
+        }
+    }
 }
 ?>
