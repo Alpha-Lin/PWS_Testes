e4805b55a1be7ade7ab0c3a7db6557d0c5f27470
Kaputchino <andreasmulard@gmail.com>
2023-11-28 14:13:05 +0100

    siplification

--- src/code_en_vrac/upload.php
+++ src/code_en_vrac/upload.php
@@ -1,26 +1,23 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+$target_file =basename($_FILES["fileToUpload"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-//Choisi le nouveau nom
+//Choisi le nouveau nom, prend le temps avtuel en miliseconde et ajoute un nb aléoa
 $newname = "img".floor(microtime(true) * 1000).rand(0,10000).'.'.$imageFileType;
 
 // verifie le bon format
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-      
+  if($check !== false) {      
     $filename = $_FILES["fileToUpload"]["tmp_name"];
       if (move_uploaded_file($filename,'uploads/'.$newname)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        echo "L'image ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " a été mise en ligne";
       }else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Problème durant l'upload";
       }
   } else {
       //si il y a un probleme
-    echo "File is not an image.";
+    echo "Pas une image";
   }
 }
 ?>
