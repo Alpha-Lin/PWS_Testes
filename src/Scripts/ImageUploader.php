716acd18f45a7b4d9bbd9d0a91324d8b66234916
Gabriel <60087391+g4bey@users.noreply.github.com>
2023-11-30 22:12:11 +0100

    fix img upload
    Co-Authored-By: Kaputchino <61330453+Kaputchino@users.noreply.github.com>

---
+++ src/Scripts/ImageUploader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace  App\Scripts;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+Class ImageUploader {
+    public $targetDirectory;
+    public $slugger;
+    
+    function __construct(?string $targetDirectory,  SluggerInterface $slugger) {
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
+    }
+
+    function upload(UploadedFile $file){
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+
+        $newname = "img_"  .$safeFilename . '_' . '.' . $file->guessExtension();
+
+        if (!getimagesize($file)) {
+          throw new ExtensionFileException ("Il y a un probleme avec l'extension.");
+        }
+
+        try { 
+            $file->move($this->targetDirectory, $newname);
+        } catch (FileException $e) {
+          throw $e;
+        }
+
+        return $newname;
+    }
+}
+?>
