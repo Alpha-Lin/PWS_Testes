182d35d35651054dbe568b3b0c53391ca41e7f63
Gabriel <60087391+g4bey@users.noreply.github.com>
2023-12-01 23:23:43 +0100

    Renamed Auth

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\auth;
+namespace App\Controller\Auth;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
