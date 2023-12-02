899c0ee620f30467379aed4fc86c633e1989bb05
Gabriel <60087391+g4bey@users.noreply.github.com>
2023-12-02 16:49:26 +0100

    updated booting scripts

---
+++ scripts/windows/reboot.ps1
@@ -0,0 +1,10 @@
+git pull 
+;  composer install 
+; npm install 
+; npm run dev 
+; npm run build 
+; symfony console doctrine:database:drop --force
+; symfony console doctrine:database:create
+; rmdir migrations/*
+; symfony console make:migration
+; symfony console doctrine:migrations:migrate
---
+++ scripts/windows/startup.ps1
@@ -0,0 +1,8 @@
+git pull 
+; composer install 
+; npm install 
+; npm run dev 
+; npm run build 
+; symfony console doctrine:database:create
+; symfony console make:migration
+; symfony console doctrine:migrations:migrate
